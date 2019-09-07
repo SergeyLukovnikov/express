d8261e3a013f1cf5a23ea7f12015dbd5853d27ab
Sergey Lukovnikov <liveloper@gmail.com>
2019-09-08 01:20:09 +0300

    added clonig repository

--- jenkinsfiles/docker-build.groovy
+++ jenkinsfiles/docker-build.groovy
@@ -11,6 +11,13 @@ pipeline {
         timestamps()
     }
     stages {
+        stage("clone repositories") {
+            steps {
+                echo " ============== start building image =================="
+                sh 'git clone https://github.com/liveloper/express.git'
+                sh 'cd express'
+            }
+        }
         stage("create docker image") {
             steps {
                 echo " ============== start building image =================="
